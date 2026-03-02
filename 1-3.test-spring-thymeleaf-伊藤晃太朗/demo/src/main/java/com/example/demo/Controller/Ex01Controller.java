2ee0764bc8c8fef08072af79d406000ec7ba04f0
Kotaro Ito <itokotaro058@gmail.com>
2026-03-02 12:12:51 +0900

    フォルダ名の変更

--- 1-3.test-spring-thymeleaf-伊藤晃太朗/demo/src/main/java/com/example/demo/Controller/Ex01Controller.java
+++ 1-3.test-spring-thymeleaf-伊藤晃太朗/demo/src/main/java/com/example/demo/Controller/Ex01Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Domain.Member;
@@ -16,7 +17,7 @@ public class Ex01Controller {
     public String index(){
         return "ex-01";
     }
-    @RequestMapping("/result")
+    @PostMapping("/result")
     public String result(String name,Integer age,ArrayList<String> hobbyList
 ,Model model){
         Member member =new Member();
