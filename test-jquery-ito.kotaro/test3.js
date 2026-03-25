147411c73f1453653eb6d074c501e458594389f1
Kotaro Ito <itokotaro058@gmail.com>
2026-03-25 10:37:40 +0900

    jQuery

---
+++ test-jquery-ito.kotaro/test3.js
@@ -0,0 +1,4 @@
+$( "button" ).on( "click", function() {
+    let value=$( "#age" ).val();
+  $(this).alert(value);
+} );
---
+++ test-jquery-ito.kotaro/test4.js
@@ -0,0 +1,3 @@
+$( "button" ).on( "click", function() {
+  $(this).prop("disabled",true);
+} );
